c8069125ecf34ad04b6cc7ed094c4070839ac854
Jonathan Guthrie <jono@perfect.org>
2017-10-03 15:24:47 -0400

    update to use swift 4/perfect 3

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-04 16:25:29 +0000
+// Date: 2017-10-03 19:14:44 +0000
 import PackageDescription
 let package = Package(
-    name: "MongoDBStORM-Demo",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", majorVersion: 1),
-    ]
+	name: "MongoDBStORM-Demo",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", majorVersion: 3),
+	]
 )
